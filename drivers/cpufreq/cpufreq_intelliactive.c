48bef3679120220a3b3b4e75e20746e2bca88b2d
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-06-28 03:48:07 -0500

    Cpufreq: Intelliactive Tweaked
    Signed-off-by: Carlos Jesús <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -68,14 +68,14 @@ static struct mutex gov_lock;
 static unsigned int hispeed_freq;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 99
+#define DEFAULT_GO_HISPEED_LOAD 97
 static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 
 /* Sampling down factor to be applied to min_sample_time at max freq */
 static unsigned int sampling_down_factor;
 
 /* Target load.  Lower values result in higher CPU speeds. */
-#define DEFAULT_TARGET_LOAD 90
+#define DEFAULT_TARGET_LOAD 85
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 static spinlock_t target_loads_lock;
 static unsigned int *target_loads = default_target_loads;
@@ -90,7 +90,7 @@ static unsigned long min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 /*
  * The sample rate of the timer used to increase frequency
  */
-#define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
+#define DEFAULT_TIMER_RATE (10 * USEC_PER_MSEC)
 static unsigned long timer_rate = DEFAULT_TIMER_RATE;
 
 /* Busy SDF parameters*/
@@ -130,10 +130,13 @@ static bool io_is_busy = 1;
  * sync_freq
  */
 static unsigned int up_threshold_any_cpu_load = 95;
-static unsigned int sync_freq = 787200;
-static unsigned int up_threshold_any_cpu_freq = 998400;
+/*static unsigned int sync_freq = 787200;
+static unsigned int up_threshold_any_cpu_freq = 998400;*/
+static unsigned int sync_freq = 998400;
+static unsigned int up_threshold_any_cpu_freq = 1190400;
 
-static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1190400} ;
+/*static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1190400} ;older*/
+static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1593600} ;
 
 static int cpufreq_governor_intelliactive(struct cpufreq_policy *policy,
 		unsigned int event);
